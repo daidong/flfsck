24c0d53478f87b00c7b8c3edccfcb885a3f506d7
dong dai <daidong@macbook-pro-3.attlocal.net>
2018-04-21 01:49:10 -0500

    update cloudlab script

--- cloudlab-build-lustre-source.sh
+++ cloudlab-build-lustre-source.sh
@@ -13,9 +13,15 @@ priority=1
 EOF
 yum update -y e2fsprogs
 
-
-yum -y localinstall {kmod-lustre-osd-ldiskfs,kmod-lustre,lustre,lustre-osd-ldiskfs-mount,lustre-iokit,lustre-tests,kmod-lustre-tests}-2.9.0_dirty-1.el7.centos.x86_64.rpm
-
 #For resolving Conflict with lustre-client-2.9.0-1.el7.x86_64 error
 # rpm -qa|grep ‘client’
-# rpm -e lustre-client-2.9.0-1.el7.x86_64
+rpm -e lustre-client-2.9.0-1.el7.x86_64
+yum -y localinstall {kmod-lustre-osd-ldiskfs,kmod-lustre,lustre,lustre-osd-ldiskfs-mount,lustre-iokit,lustre-tests,kmod-lustre-tests}-2.9.0-1.el7.centos.x86_64.rpm
+echo 'options lnet networks=tcp0(p2p2)' > /etc/modprobe.d/lustre.conf
+depmod -a
+modprobe lustre
+lctl set_param debug=+lfsck
+lctl set_param printk=+lfsck
+
+
+rpm -e kmod-lustre-osd-ldiskfs-2.9.0-1.el7.centos.x86_64 lustre-osd-ldiskfs-mount-2.9.0-1.el7.centos.x86_64 kmod-lustre-2.9.0-1.el7.centos.x86_64 kmod-lustre-tests-2.9.0-1.el7.centos.x86_64 lustre-2.9.0-1.el7.centos.x86_64 lustre-tests-2.9.0-1.el7.centos.x86_64 lustre-iokit-2.9.0-1.el7.centos.x86_64
